659b8fe608a8b86a01b81f7d9942932d76872353
Sávio Gomes da Silva <saviogomesdasilva@yahoo.com>
2023-11-28 10:40:31 -0300

    colocando try

--- conexao.php
+++ conexao.php
@@ -1,7 +1,14 @@
 <?php
-$servidor = "localhost";
-$usuario = "root";
-$senha = "";
-$dbname = "testephp";
-
-$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+try {
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $dbname = "testephp";
+    if ($conn) {
+        $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+    } else {
+        mysqli_connect_error();
+    }
+} catch (\Throwable $th) {
+    throw $th;
+}
